e77391592362ce09fc281735258cdefb552f2172
Knut Waale <kwaale@github.com>
2021-09-23 15:36:34 -0300

    Preparando para deploy

--- src/components/Dados/Dado.js
+++ src/components/Dados/Dado.js
@@ -1,7 +1,12 @@
 import './Dados.css';
+import { useEffect } from 'react';
 
 function Dado(props){
-  console.log(props)
+  console.log('Componente Dado')
+  useEffect(()=>{
+    setTimeout(console.log('Componente Dado timer'),4000)
+    clearTimeout()
+  },[])
   return(
     <img className='Dado-recuadro' src={props.image}/>
   )
--- src/components/Dados/index.jsx
+++ src/components/Dados/index.jsx
@@ -2,6 +2,12 @@ import Dado from "./Dado.js"
 import './Dados.css'
 import { useState, useEffect } from "react";
 import Totallizer from "../Totallizer/index.jsx";
+import dado1 from '../../img/dados/1.png'
+import dado2 from '../../img/dados/2.png'
+import dado3 from '../../img/dados/3.png'
+import dado4 from '../../img/dados/4.png'
+import dado5 from '../../img/dados/5.png'
+import dado6 from '../../img/dados/6.png'
 
 
 const Dados = ({input})=>{
@@ -10,12 +16,12 @@ const Dados = ({input})=>{
     const randomCrap = () =>{
         // let image = ''
         const num = Math.floor(Math.random() * 6) + 1
-        if(num == 1) return {image:'https://picsum.photos/200/300?random=2', num}
-        if(num == 2) return {image:'https://picsum.photos/200/300?random=2', num}
-        if(num == 3) return {image:'https://picsum.photos/200/300?random=2', num}
-        if(num == 4) return {image:'https://picsum.photos/200/300?random=2', num}
-        if(num == 5) return {image:'https://picsum.photos/200/300?random=2', num}
-        else return {image:'https://picsum.photos/200/300?random=2', num}
+        if(num === 1) return {image:dado1, num}
+        if(num === 2) return {image:dado2, num}
+        if(num === 3) return {image:dado3, num}
+        if(num === 4) return {image:dado4, num}
+        if(num === 5) return {image:dado5, num}
+        else return {image:dado6, num}
     }
     
     const idGenerator = function (){
@@ -29,29 +35,33 @@ const Dados = ({input})=>{
     const quantityCraps = (num)=>{
         let array = [];
         for(let i = 0; i < num; i++){
+            const val = randomCrap()
             let obj = {
                 id:idGenerator(),
-                num:randomCrap().num,
-                image:randomCrap().image
+                num:val.num,
+                image:val.image
             }
             totallizer += obj.num;
             setTotal(totallizer)
             array.push(obj);
         }
-        setCraps(array)
+        setTimeout(setCraps(array),4000)
     }
     const lanzarDados = () =>{
         quantityCraps(input)
+    }
+    const dadosAndando = (craps)=>{
+        return craps.map(dado => {
+            return (
+                <Dado key={dado.id} valor={dado.num} image={dado.image}/>
+            )
+        })
     }
     return(
         <div className='dados-container'>
                 {total && <Totallizer total={total} />}
                 <div className='craps-container'>
-                {craps.map(dado => {
-                    return (
-                        <Dado key={dado.id} valor={dado.num} image={dado.image}/>
-                    )
-                })}
+                {dadosAndando(craps)}
             </div>
             {total && <button className='btn-lanzar' onClick={lanzarDados}>Lanzar</button>}
         </div>
