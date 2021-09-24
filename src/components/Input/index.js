bc0387e6811a3bbdde8d4a99df14483db29db701
Knut Waale <kwaale@github.com>
2021-09-24 14:42:20 -0300

    Local storage

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,21 +4,19 @@ import './Input.css';
 
 
 const Input = () => {
-    const [craps, setCraps] = useState(0);
-
+    const [craps, setCraps] = useState(parseInt(JSON.parse(localStorage.getItem('dados'))) || 0);
+    localStorage.setItem('dados', JSON.stringify(craps))
     const crapsCuantity = (e) => {
         if(!e.target.value)return setCraps(parseInt(0))
         if(isNaN(e.target.value))return
         const value = parseInt(e.target.value);
         setCraps(parseInt(value) && parseInt(value))
-
     }
     const handlerCrapsNumber = (sum)=>{
         if(sum === '-' && craps > 0)setCraps(craps - 1)
-        if(sum === '+' && craps < 2000)setCraps(craps + 1)
-        
+        if(sum === '+' && craps < 2000)setCraps(craps + 1)   
     }
-
+    
     return (
         <div className='containers'>
             <div>
